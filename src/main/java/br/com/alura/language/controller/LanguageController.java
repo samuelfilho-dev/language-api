b45fc90835591dbfae74bd03ca2bb7bbb2f8e7b9
Samuel Marques <snamuelfilho2011@gmail.com>
2023-03-31 21:51:05 -0300

    Desafios da Aula 4

--- src/main/java/br/com/alura/language/controller/LanguageController.java
+++ src/main/java/br/com/alura/language/controller/LanguageController.java
@@ -3,35 +3,77 @@ package br.com.alura.language.controller;
 import br.com.alura.language.model.Language;
 import br.com.alura.language.repository.LanguageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class LanguageController {
 
     private final LanguageRepository repository;
 
     @GetMapping("/languages")
-    public ResponseEntity<List<Language>> getAllLanguages(){
+    public ResponseEntity<Page<Language>> getAllLanguages(Pageable pageable){
 
-        List<Language> languages = repository.findAll();
+        log.info("Get All Languages");
+
+        Page<Language> languages = repository.findByOrderByRanking(pageable);
 
         return ResponseEntity.ok(languages);
+    }
+
+    @GetMapping("/languages/{id}")
+    public ResponseEntity<Language> getById(@PathVariable String id){
+
+        log.info("Get Language By Id");
+
+        Language language = repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Language not found"));
+
+        return ResponseEntity.ok(language);
     }
 
     @PostMapping("/languages")
     public ResponseEntity<Language> createLanguage(@RequestBody Language language){
 
+        log.info("Create new Language ");
+
         Language saved = repository.save(language);
 
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/languages/{id}")
+    public ResponseEntity<Language> upgradeLanguage(@PathVariable String id, @RequestBody Language language){
+
+        log.info("Upgrade a Language By Id");
+
+        Language lg = repository.findById(id).get();
+
+        lg.setTitle(language.getTitle());
+        lg.setImage(language.getImage());
+        lg.setRanking(language.getRanking());
+
+        Language save = repository.save(lg);
+
+        return ResponseEntity.ok(save);
+    }
+
+    @DeleteMapping("/languages/{id}")
+    public ResponseEntity<Void> deleteLanguage(@PathVariable String id){
+
+        log.info("Delete a Language By Id");
+
+        Language lg = repository.findById(id).get();
+
+        repository.delete(lg);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/com/alura/language/repository/LanguageRepository.java
+++ src/main/java/br/com/alura/language/repository/LanguageRepository.java
@@ -1,10 +1,13 @@
 package br.com.alura.language.repository;
 
 import br.com.alura.language.model.Language;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LanguageRepository extends MongoRepository<Language, String> {
 
+    Page<Language> findByOrderByRanking(Pageable pageable);
 }
